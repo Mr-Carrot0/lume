eead638ef08fbacc76392c9eb7cdfc20c2834236
Mr. Carrot <177513746+Mr-Carrot0@users.noreply.github.com>
2025-09-15 15:28:02 +0200

    Update better-robots.ts

--- plugins/better-robots.ts
+++ plugins/better-robots.ts
@@ -1,110 +1,125 @@
-import { merge } from "lume/core/utils/object.ts";
+import { merge } from "../core/utils/object.ts";
 
-import type Site from "lume/core/site.ts";
-
-type optArr<T> = T | T[];
+import type Site from "../core/site.ts";
 
 type Rule = Partial<{
-    /** User-agent */
-    userAgent: optArr<string>;
-    /** Crawl-delay */
-    crawlDelay?: string;
-    /** Disallow */
-    disallow: optArr<string>;
-    /** Disavow */
-    disavow: string; // no idea what that is tbh :p
-    /** Allow */
-    allow: optArr<string>;
-    /** Host */
-    host: string;
-    /** Sitemap */
-    sitemap: string;
-    /** Clean-param */
-    cleanParam: string;
+  /** User-agent */
+  userAgent: string | string[];
+  /** Crawl-delay */
+  crawlDelay: string;
+  /** Disallow */
+  disallow: string | string[];
+  /** Disavow */
+  disavow: string; // no idea what that is tbh :p
+  /** Allow */
+  allow: string | string[];
+  /** Host */
+  host: string;
+  /** Sitemap */
+  sitemap: string;
+  /**
+   * Clean-param
+   * @see https://yandex.com/support/webmaster/en/robot-workings/clean-param
+   */
+  cleanParam: string;
 }>;
 
 const ruleSort = [
-    "userAgent",
-    "crawlDelay",
-    "disallow",
-    "disavow",
-    "allow",
-    "host",
-    "sitemap",
-    "cleanParam",
+  "userAgent",
+  "crawlDelay",
+  "disallow",
+  "disavow",
+  "allow",
+  "host",
+  "sitemap",
+  "cleanParam",
 ];
 
 export interface Options {
-    /** The robots.txt file name */
-    filename: string;
+  /** The robots.txt file name */
+  filename: string;
 
-    /** User-agent to allow */
-    allow?: optArr<string>;
+  /** User-agent to allow */
+  allow?: string | string[];
 
-    /** User-agent to disallow */
-    disallow?: optArr<string>;
+  /** User-agent to disallow */
+  disallow?: string | string[];
 
-    /** Custom rules */
-    rules?: Rule[];
+  /** Custom rules */
+  rules?: Rule[];
 }
 
 export const defaults: Options = {
-    filename: "/robots.txt",
-    allow: "*",
+  filename: "/robots.txt",
+  allow: "*",
 };
 
-export function brobots(userOptions?: Partial<Options>) {
-    const options: Options = merge(defaults, userOptions);
-    return (site: Site) => {
-        site.process(async function () {
-            const rules: Rule[] = [];
-            const allow = typeof options.allow === "string"
-                ? [options.allow]
-                : options.allow;
-            const disallow = typeof options.disallow === "string"
-                ? [options.disallow]
-                : options.disallow;
+/**
+ * A plugin to generate a robots.txt after build
+ * @see https://lume.land/plugins/robots/
+ */
+export function robots(userOptions?: Partial<Options>) {
+  const options: Options = merge(defaults, userOptions);
+  return (site: Site) => {
+    site.process(async function () {
+      const rules: Rule[] = [];
+      const allow = typeof options.allow === "string"
+        ? [options.allow]
+        : options.allow;
+      const disallow = typeof options.disallow === "string"
+        ? [options.disallow]
+        : options.disallow;
 
-            [allow, disallow].forEach((_llow, i) => {
-                _llow?.forEach((userAgent) => {
-                    rules.push({
-                        userAgent,
-                        [["allow", "disallow"][i]]: "/",
-                    });
-                });
-            });
+      allow?.forEach((userAgent) =>
+        rules.push({
+          userAgent,
+          allow: "/",
+        })
+      );
 
-            rules.push(...options.rules ?? []);
+      disallow?.forEach((userAgent) =>
+        rules.push({
+          userAgent,
+          disallow: "/",
+        })
+      );
 
-            const robotFile = await site.getOrCreatePage(options.filename);
-            let rawContent = robotFile.content
-            if (rawContent && typeof rawContent === "object") {
-                rawContent = new TextDecoder().decode(rawContent);
-            }
-            const existingContent = rawContent ? `${rawContent}` : "";
+      rules.push(...options.rules ?? []);
 
-            const robotify = (key: string) =>
-                `${key.charAt(0).toUpperCase()}${key.slice(1).replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase()
-                }`;
+      // Create the robots.txt page
+      const robotFile = await site.getOrCreatePage(options.filename);
 
-            const newContent = rules.map((rule) =>
-                Object.entries(rule).sort(([keyA], [KeyB]) =>
-                    ruleSort.indexOf(keyA) - ruleSort.indexOf(KeyB)
-                ).map(([k, value]) => {
-                    const key = robotify(k);
-                    if (typeof value === "object") {
-                        // UserAgent Array
-                        return value.map((val) => `${key}: ${val}`).join("\n");
-                    } else {
-                        // anything else
-                        return `${key}: ${value}`;
-                    }
-                }).join("\n")
-            ).join("\n\n");
+      // sometimes content returned an Unit8Array
+      let rawContent = robotFile.content;
+      if (rawContent && typeof rawContent === "object") {
+        rawContent = new TextDecoder().decode(rawContent);
+      }
+      const existingContent = rawContent ? `${rawContent}` : "";
 
-            robotFile.content = newContent +"\n\n"+ existingContent;
-        });
-    };
+      const robotKey = (key: string) =>
+        `${key.charAt(0).toUpperCase()}${
+          key.slice(1)
+            .replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase()
+        }`;
+
+      const newContent = rules.map((rule) =>
+        Object.entries(rule).sort(([keyA], [KeyB]) =>
+          ruleSort.indexOf(keyA) - ruleSort.indexOf(KeyB)
+        ).map(([k, value]) => {
+          const key = robotKey(k);
+          if (typeof value === "object") {
+            // typeof value === UserAgent[]
+            return value.map((val) => `${key}: ${val}`).join("\n");
+          } else {
+            // anything else
+            return `${key}: ${value}`;
+          }
+        }).join("\n")
+      ).join("\n\n");
+
+      robotFile.content = newContent + "\n\n" + existingContent;
+    });
+  };
 }
 
-export default brobots;
+export default robots;
